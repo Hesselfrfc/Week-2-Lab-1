c9a069cca9d35ec7c64515f229b4348261cfb625
Hessel <Hesselfeyenoord@icloud.com>
2019-09-09 14:29:56 +0200

    Opdracht Lab 1 - basis toegevoegd

---
+++ b1w2l1.js
@@ -0,0 +1,5 @@
+alert('Vul de volgende 3 gegevens in');
+var naam = prompt('Vul je voornaam in');
+var achternaam = prompt('Vul je achternaam in');
+var leeftijd = prompt('Vul je leeftijd in');
+document.write (naam + ' ' + achternaam + ' ' + leeftijd);
